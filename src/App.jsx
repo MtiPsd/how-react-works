5e97ff83733d7a8ea309ba0f4b70ad17410d3a3c
Mahdi Pasandideh <mtipsd.git@gmail.com>
2023-09-11 22:20:11 +0330

    learned about batching

--- src/App.jsx
+++ src/App.jsx
@@ -39,7 +39,10 @@ function Tabbed({ content }) {
       </div>
 
       {activeTab <= 2 ? (
-        <TabContent item={content.at(activeTab)} />
+        <TabContent
+          item={content.at(activeTab)}
+          key={content.at(activeTab).summary}
+        />
       ) : (
         <DifferentContent />
       )}
@@ -62,8 +65,31 @@ function TabContent({ item }) {
   const [showDetails, setShowDetails] = useState(true);
   const [likes, setLikes] = useState(0);
 
+  console.log('RENDER');
+
   function handleInc() {
-    setLikes(likes + 1);
+    setLikes(likes => likes + 1);
+    // this might work as well but we never
+    // know what we will do with this function
+    // in the future
+    // setLikes(likes);
+  }
+
+  function handleTripleInc() {
+    setLikes(likes => likes + 1);
+    setLikes(likes => likes + 1);
+    setLikes(likes => likes + 1);
+  }
+
+  function handleUndo() {
+    // these two states are batched
+    // so they will cause only one render
+    setShowDetails(true);
+    setLikes(0);
+  }
+
+  function handleUndoLater(e) {
+    setTimeout(handleUndo, 2000);
   }
 
   return (
@@ -79,13 +105,13 @@ function TabContent({ item }) {
         <div className='hearts-counter'>
           <span>{likes} ❤️</span>
           <button onClick={handleInc}>+</button>
-          <button>+++</button>
+          <button onClick={handleTripleInc}>+++</button>
         </div>
       </div>
 
       <div className='tab-undo'>
-        <button>Undo</button>
-        <button>Undo in 2s</button>
+        <button onClick={handleUndo}>Undo</button>
+        <button onClick={handleUndoLater}>Undo in 2s</button>
       </div>
     </div>
   );
@@ -98,6 +124,3 @@ function DifferentContent() {
     </div>
   );
 }
-
-// made custom theme for vs code
-// I learned a lot
